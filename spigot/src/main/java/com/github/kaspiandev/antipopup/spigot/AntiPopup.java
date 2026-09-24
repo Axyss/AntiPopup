6f0b2f65d3345e619979d120e57a4b0dce4d1c37
Axyss <contact@axyss.dev>
2026-09-24 23:22:45 +0200

    Add missing logic to support PlayerInjector_v26_3

--- spigot/src/main/java/com/github/kaspiandev/antipopup/spigot/AntiPopup.java
+++ spigot/src/main/java/com/github/kaspiandev/antipopup/spigot/AntiPopup.java
@@ -20,6 +20,7 @@ import com.github.kaspiandev.antipopup.nms.v1_21_9.PlayerInjector_v1_21_9;
 import com.github.kaspiandev.antipopup.nms.v1_21_11.PlayerInjector_v1_21_11;
 import com.github.kaspiandev.antipopup.nms.v26_1.PlayerInjector_v26_1;
 import com.github.kaspiandev.antipopup.nms.v26_2.PlayerInjector_v26_2;
+import com.github.kaspiandev.antipopup.nms.v26_3.PlayerInjector_v26_3;
 import com.github.kaspiandev.antipopup.spigot.api.Api;
 import com.github.kaspiandev.antipopup.spigot.hook.HookManager;
 import com.github.kaspiandev.antipopup.spigot.hook.viaversion.ViaVersionHook;
@@ -127,6 +128,7 @@ public final class AntiPopup extends JavaPlugin {
         if (config.isBlockChatReports()) {
             if (!config.isExperimentalMode()) {
                 PlayerListener playerListener = switch (serverManager.getVersion()) {
+                    case V_26_3 -> new PlayerListener(new PlayerInjector_v26_3());
                     case V_26_2 -> new PlayerListener(new PlayerInjector_v26_2());
                     case V_26_1, V_26_1_1, V_26_1_2 -> new PlayerListener(new PlayerInjector_v26_1());
                     case V_1_21_11 -> new PlayerListener(new PlayerInjector_v1_21_11());
